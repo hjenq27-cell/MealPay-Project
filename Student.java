1938df28555c8a3363683b77fb05a1b7b7316fab
Kyan Chiang Zanoria <kchiang-zanoria27@students.polytechnic.org>
2026-02-10 12:04:18 -0800

    made fields and constructor for student object

--- Student.java
+++ Student.java
@@ -1,4 +1,17 @@
 public class Student{
+    public String name;
+    public int ID;
+    public double balance;
+    public arrayList <Transaction>;
+    private static int ID_Gen = 0;
+    
+    public Student(String myname){
+        name = myname;
+        ID = ID_Gen;
+        ID_Gen++;
+    }
+    
+    
 
 
 
