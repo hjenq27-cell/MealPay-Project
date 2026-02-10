1f908a6329b288cd66e30c7a5436776e5da5bc58
Kyan Chiang Zanoria <kchiang-zanoria27@students.polytechnic.org>
2026-02-10 12:16:40 -0800

    made getters and toString  + fixed fields

--- Student.java
+++ Student.java
@@ -3,15 +3,33 @@ public class Student{
     public String name;
     public int ID;
     public double balance;
-    public ArrayList <Transaction>Transactions;
+    public ArrayList <Transaction> Transactions;
     private static int ID_Gen = 0;
     
     public Student(String myname){
         name = myname;
         ID = ID_Gen;
         ID_Gen++;
+        balance = 0;
+        Transactions = new ArrayList<Transaction>();
     }
     
+    public String getName(){
+        return name;
+    }
+
+    public double getBalance(){
+        return balance;
+    }
+
+    public int getID(){
+        return ID;
+    }
+
+    public String toString(){
+        return "Name: " + name + " ID: " + ID + " Balance: " + balance + " Transactions: " + Transactions;
+    }
+
     
 
 
