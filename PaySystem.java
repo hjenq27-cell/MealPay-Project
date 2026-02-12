850dbba18e846f5f9cc46be96fd5aa31fc744a4f
Emilio Doust <edoust27@students.polytechnic.org>
2026-02-12 13:16:40 -0800

    delete stuff

--- PaySystem.java
+++ PaySystem.java
@@ -2,21 +2,16 @@ import java.util.ArrayList;
 
 public class PaySystem{
 
-    private ArrayList<Student> students;
+    private ArrayList<Student> students = new ArrayList<>();
     
-    private static int ID_Gen = 1;
-
     public void addStudent(String name){
-        Student newStudent = new Student(name, ID_Gen);
-        ID_Gen++;
-        students.add(newStudent);
-
+       
     }
+}
 
     public void addFunds(int idNo, int amount){
-        while(true){
-            
-        }
+
+
     }
 
     public void chargeLunch(int idNo){
